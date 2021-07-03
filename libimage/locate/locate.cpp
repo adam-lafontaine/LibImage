e8cef2bb29e3929de0d02ad4c85f9cffb68673f3
adam-lafontaine <adam.lafontaine@gmail.com>
2021-07-03 12:10:18 -0400

    edge delta and find edges

--- libimage/locate/locate.cpp
+++ libimage/locate/locate.cpp
@@ -9,6 +9,12 @@
 #include <functional>
 #include <array>
 
+
+// outer edge pixels of src are ignored
+constexpr u32 DST_OFFSET = 1;
+constexpr u32 DELTA_DIM = 2 * DST_OFFSET;
+constexpr u32 map_to_src(u32 dst_i) { return dst_i + DST_OFFSET; }
+
 namespace libimage
 {
 	static void q_edges(gray::view_t const& src, gray::view_t const& dst, u8 threshold)
@@ -19,9 +25,8 @@ namespace libimage
 		u32 const dst_h = dst.height;
 
 		// outer edge pixels of src are ignored
-		assert(src_w - dst_w == 2u);
-		assert(src_h - dst_h == 2u);
-		constexpr auto map_src = [](u32 i) { return i + 1; };
+		assert(src_w - dst_w == DELTA_DIM);
+		assert(src_h - dst_h == DELTA_DIM);
 
 		// get gradient magnitude of inner pixels
 		u32 const dst_x_begin = 0;
@@ -38,8 +43,8 @@ namespace libimage
 
 			auto const grad_x = [&](u32 dst_x)
 			{
-				auto src_x = map_src(dst_x);
-				auto src_y = map_src(dst_y);
+				auto src_x = map_to_src(dst_x);
+				auto src_y = map_to_src(dst_y);
 				auto gx = x_gradient(src, src_x, src_y);
 				auto gy = y_gradient(src, src_x, src_y);
 				auto g = std::hypot(gx, gy);
@@ -53,5 +58,99 @@ namespace libimage
 	}
 
 
+	static u32 edge_delta(gray::view_t const& lhs, gray::view_t const& rhs)
+	{
+		assert(lhs.width == rhs.width);
+		assert(lhs.height == rhs.height);
 
+		u32 const width = lhs.width;
+		u32 const height = rhs.height;
+
+		u32 total = 0;
+
+		auto const add = [&total](u32 x, u32 y) { total += 0; };
+
+		for (u32 y = 0; y < height; ++y)
+		{
+			auto lhs_row = lhs.row_begin(y);
+			auto rhs_row = rhs.row_begin(y);
+			for (u32 x = 0; x < width; ++x)
+			{
+				total += (lhs_row[x] != rhs_row[x]);
+			}
+		}
+
+		return total;
+	}
+
+
+	static gray::view_t find_edges(gray::view_t const& view)
+	{
+		u32 const width = view.width;
+		u32 const height = view.height;
+
+		pixel_range_t r = { 0, 0, 0, 0 };		
+
+		// search from top
+		bool found = false;
+		for (u32 y = 0; y < height && !found; ++y)
+		{
+			auto row = view.row_begin(y);
+			for (u32 x = 0; x < width && !found; ++x)
+			{
+				if (row[x])
+				{
+					r.y_begin = y;
+					found = true;
+				}				
+			}
+		}
+
+		// search from bottom
+		found = false;
+		for (u32 y = height; y > 0 && !found; --y)
+		{
+			auto row = view.row_begin(y - 1);
+			for (u32 x = width; x > 0 && !found; --x)
+			{
+				if (row[x - 1])
+				{
+					r.y_end = y;
+					found = true;
+				}
+			}
+		}
+
+		// search from left
+		found = false;
+		for (u32 x = 0; x < width && !found; ++x)
+		{
+			for (u32 y = 0; y < height && !found; ++y)
+			{
+				if (view.row_begin(y)[x])
+				{
+					r.x_begin = x;
+					found = true;
+				}
+			}
+		}
+
+		// search from right
+		found = false;
+		for (u32 x = width; x > 0 && !found; --x)
+		{
+			for (u32 y = height; y > 0 && !found; --y)
+			{
+				if (view.row_begin(y - 1)[x - 1])
+				{
+					r.x_end = y;
+				}
+			}
+		}
+
+		assert(r.x_end > r.x_begin);
+		assert(r.y_end > r.y_begin);
+
+		return sub_view(view, r);
+	}
 }
