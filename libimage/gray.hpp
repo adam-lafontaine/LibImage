f5c3d4aa56c215f6d1a65e4aec87814473b80709
adam-lafontaine <adam.lafontaine@gmail.com>
2021-07-10 07:25:23 -0400

    gray row and column view

--- libimage/gray.hpp
+++ libimage/gray.hpp
@@ -15,7 +15,7 @@ namespace libimage
 
 
 		// grayscale image
-		class image_t
+		class Image
 		{
 		public:
 			u32 width = 0;
@@ -50,7 +50,7 @@ namespace libimage
 				}
 			}
 
-			~image_t()
+			~Image()
 			{
 				clear();
 			}
@@ -68,7 +68,7 @@ namespace libimage
 
 
 		// subset of grayscale image data
-		class image_view_t
+		class ImageView
 		{
 		public:
 
@@ -150,7 +150,7 @@ namespace libimage
 
 				explicit iterator() {}
 
-				explicit iterator(image_view_t const& view)
+				explicit iterator(ImageView const& view)
 				{
 					image_data = view.image_data;
 					image_width = view.image_width;
@@ -163,6 +163,8 @@ namespace libimage
 					loc_x = x_begin;
 					loc_y = y_begin;
 				}
+
+				xy_loc_t get_xy() { return { loc_x, loc_y }; }
 
 				iterator end()
 				{
@@ -178,9 +180,7 @@ namespace libimage
 					next();
 
 					return *this;
-				}
-
-				xy_loc_t get_xy() { return { loc_x, loc_y }; }
+				}				
 
 				iterator operator ++ (int) { iterator result = *this; ++(*this); return result; }
 
@@ -201,6 +201,137 @@ namespace libimage
 			iterator end() const { return iterator(*this).end(); }
 		};
 
-		using view_t = image_view_t;
+		
+
+
+		class ImageRowView
+		{
+		public:
+
+			pixel_t* image_data = 0;
+			u32 image_width = 0;
+
+			u32 x_begin = 0;
+			u32 x_end = 0;
+			u32 y_begin = 0;
+
+			u32 width = 0;
+
+			pixel_t* begin() { return image_data + static_cast<u64>(y_begin * image_width + x_begin); }
+
+			pixel_t* end() { return image_data + static_cast<u64>(y_begin * image_width + x_end); }
+
+			pixel_t* begin() const { return image_data + static_cast<u64>(y_begin * image_width + x_begin); }
+
+			pixel_t* end() const { return image_data + static_cast<u64>(y_begin * image_width + x_end); }
+		};
+
+
+		class ImageColumnView
+		{
+		public:
+
+			pixel_t* image_data = 0;
+			u32 image_width = 0;
+
+			u32 x_begin = 0;			
+			u32 y_begin = 0;
+			u32 y_end = 0;
+
+			class iterator
+			{
+			private:
+
+				u32 loc_x = 0;
+				u32 loc_y = 0;
+
+				u32 y_begin = 0;
+				u32 y_end = 0;
+
+				pixel_t* image_data = 0;
+				u32 image_width = 0;
+
+				pixel_t* loc_ptr() const
+				{
+					assert(loc_y >= y_begin);
+					assert(loc_y < y_end);
+
+					auto offset = loc_y * image_width + loc_x;
+					auto ptr = image_data + static_cast<u64>(offset);
+					assert(ptr);
+
+					return ptr;
+				}
+
+				void next()
+				{
+					++loc_y;
+
+					assert(loc_y >= y_begin);
+					assert(loc_y <= y_end);
+				}				
+
+			public:
+
+				using iterator_category = std::forward_iterator_tag;
+				using value_type = pixel_t;
+				using difference_type = std::ptrdiff_t;
+				using pointer = value_type*;
+				using reference = value_type&;
+
+				explicit iterator() {}
+
+				explicit iterator(ImageColumnView const& view)
+				{
+					image_data = view.image_data;
+					image_width = view.image_width;
+
+					y_begin = view.y_begin;
+					y_end = view.y_end;
+
+					loc_x = view.x_begin;
+					loc_y = y_begin;
+				}
+
+				xy_loc_t get_xy() { return { loc_x, loc_y }; }
+
+				iterator end()
+				{
+					loc_y = y_end;
+
+					return *this;
+				}
+
+				iterator& operator ++ ()
+				{
+					next();
+
+					return *this;
+				}
+
+				iterator operator ++ (int) { iterator result = *this; ++(*this); return result; }
+
+				bool operator == (iterator other) const { return loc_x == other.loc_x && loc_y == other.loc_y; }
+
+				bool operator != (iterator other) const { return !(*this == other); }
+
+				reference operator * () const { return *loc_ptr(); }
+			};
+
+			iterator begin() { return iterator(*this); }
+
+			iterator end() { return iterator(*this).end(); }
+
+			iterator begin() const { return iterator(*this); }
+
+			iterator end() const { return iterator(*this).end(); }
+		};
+
+
+
+		using image_t = Image;
+		using view_t = ImageView;
+		using row_view_t = ImageRowView;
+		using column_view_t = ImageColumnView;
 	}
 }
