300dfa888f70727837bd015deb95db265f3e914c
adam-lafontaine <adam.lafontaine@gmail.com>
2021-10-07 19:25:26 -0400

    overload convolve

--- libimage/proc/convolve.cpp
+++ libimage/proc/convolve.cpp
@@ -90,16 +90,16 @@ namespace libimage
 
 
 	template<size_t N>
-	static r32 apply_weights(gray::view_t const& img, std::array<r32, N> weights)
+	static r32 apply_weights(gray::view_t const& view, std::array<r32, N> weights)
 	{
-		assert(static_cast<size_t>(img.width) * img.height == weights.size());
+		assert(static_cast<size_t>(view.width) * view.height == weights.size());
 
 		u32 w = 0;
 		r32 total = 0.0f;
 
 		auto const add_weight = [&](u8 p) { total += weights[w++] * p; };
 
-		std::for_each(img.begin(), img.end(), add_weight);
+		std::for_each(view.begin(), view.end(), add_weight);
 
 		return total;
 	}
@@ -118,111 +118,139 @@ namespace libimage
 	}
 
 
-	static r32 weighted_center(gray::view_t const& view, u32 x, u32 y, std::array<r32, 25> const& weights)
+	template<class GRAY_IMAGE_T>
+	static r32 weighted_center(GRAY_IMAGE_T const& img, u32 x, u32 y, std::array<r32, 25> const& weights)
 	{
 		pixel_range_t range = {};
 
-		top_or_bottom_5_high(range, y, view.height);
+		top_or_bottom_5_high(range, y, img.height);
 
-		left_or_right_5_wide(range, x, view.width);
+		left_or_right_5_wide(range, x, img.width);
 
-		return apply_weights(sub_view(view, range), weights);
+		return apply_weights(sub_view(img, range), weights);
 	}
 
 
-	static r32 weighted_top_left(gray::view_t const& view, u32 x, u32 y, std::array<r32, 4> const& weights)
+	template<class GRAY_IMAGE_T>
+	static r32 weighted_top_left(GRAY_IMAGE_T const& img, u32 x, u32 y, std::array<r32, 4> const& weights)
 	{
 		pixel_range_t range = {};
 
-		top_2_high(range, y, view.height);
+		top_2_high(range, y, img.height);
 
-		left_2_wide(range, x, view.width);
+		left_2_wide(range, x, img.width);
 
-		return apply_weights(sub_view(view, range), weights);
+		return apply_weights(sub_view(img, range), weights);
 	}
 
 
-	static r32 weighted_top_right(gray::view_t const& view, u32 x, u32 y, std::array<r32, 4> const& weights)
+	template<class GRAY_IMAGE_T>
+	static r32 weighted_top_right(GRAY_IMAGE_T const& img, u32 x, u32 y, std::array<r32, 4> const& weights)
 	{
 		pixel_range_t range = {};
 
-		top_2_high(range, y, view.height);
+		top_2_high(range, y, img.height);
 
-		right_2_wide(range, x, view.width);
+		right_2_wide(range, x, img.width);
 
-		return apply_weights(sub_view(view, range), weights);
+		return apply_weights(sub_view(img, range), weights);
 	}
 
 
-	static r32 weighted_bottom_left(gray::view_t const& view, u32 x, u32 y, std::array<r32, 4> const& weights)
+	template<class GRAY_IMAGE_T>
+	static r32 weighted_bottom_left(GRAY_IMAGE_T const& img, u32 x, u32 y, std::array<r32, 4> const& weights)
 	{
 		pixel_range_t range = {};
 
-		bottom_2_high(range, y, view.width);
+		bottom_2_high(range, y, img.width);
 
-		left_2_wide(range, x, view.width);
+		left_2_wide(range, x, img.width);
 
-		return apply_weights(sub_view(view, range), weights);
+		return apply_weights(sub_view(img, range), weights);
 	}
 
 
-	static r32 weighted_bottom_right(gray::view_t const& view, u32 x, u32 y, std::array<r32, 4> const& weights)
+	template<class GRAY_IMAGE_T>
+	static r32 weighted_bottom_right(GRAY_IMAGE_T const& img, u32 x, u32 y, std::array<r32, 4> const& weights)
 	{
 		pixel_range_t range = {};
 
-		bottom_2_high(range, y, view.width);
+		bottom_2_high(range, y, img.width);
 
-		right_2_wide(range, x, view.width);
+		right_2_wide(range, x, img.width);
 
-		return apply_weights(sub_view(view, range), weights);
+		return apply_weights(sub_view(img, range), weights);
 	}
 
 
-	static r32 weighted_top(gray::view_t const& view, u32 x, u32 y, std::array<r32, 6> const& weights)
+	template<class GRAY_IMAGE_T>
+	static r32 weighted_top(GRAY_IMAGE_T const& img, u32 x, u32 y, std::array<r32, 6> const& weights)
 	{
 		pixel_range_t range = {};
 
-		top_2_high(range, y, view.height);
+		top_2_high(range, y, img.height);
 
-		left_or_right_3_wide(range, x, view.width);
+		left_or_right_3_wide(range, x, img.width);
 
-		return apply_weights(sub_view(view, range), weights);
+		return apply_weights(sub_view(img, range), weights);
 	}
 
 
-	static r32 weighted_bottom(gray::view_t const& view, u32 x, u32 y, std::array<r32, 6> const& weights)
+	template<class GRAY_IMAGE_T>
+	static r32 weighted_bottom(GRAY_IMAGE_T const& img, u32 x, u32 y, std::array<r32, 6> const& weights)
 	{
 		pixel_range_t range = {};
 
-		bottom_2_high(range, y, view.width);
+		bottom_2_high(range, y, img.width);
 
-		left_or_right_3_wide(range, x, view.width);
+		left_or_right_3_wide(range, x, img.width);
 
-		return apply_weights(sub_view(view, range), weights);
+		return apply_weights(sub_view(img, range), weights);
 	}
 
 
-	static r32 weighted_left(gray::view_t const& view, u32 x, u32 y, std::array<r32, 6> const& weights)
+	template<class GRAY_IMAGE_T>
+	static r32 weighted_left(GRAY_IMAGE_T const& img, u32 x, u32 y, std::array<r32, 6> const& weights)
 	{
 		pixel_range_t range = {};
 
-		top_or_bottom_3_high(range, y, view.height);
+		top_or_bottom_3_high(range, y, img.height);
 
-		left_2_wide(range, x, view.width);
+		left_2_wide(range, x, img.width);
 
-		return apply_weights(sub_view(view, range), weights);
+		return apply_weights(sub_view(img, range), weights);
 	}
 
 
-	static r32 weighted_right(gray::view_t const& view, u32 x, u32 y, std::array<r32, 6> const& weights)
+	template<class GRAY_IMAGE_T>
+	static r32 weighted_right(GRAY_IMAGE_T const& img, u32 x, u32 y, std::array<r32, 6> const& weights)
 	{
 		pixel_range_t range = {};
 
-		top_or_bottom_3_high(range, y, view.height);
+		top_or_bottom_3_high(range, y, img.height);
 
-		right_2_wide(range, x, view.width);
+		right_2_wide(range, x, img.width);
 
-		return apply_weights(sub_view(view, range), weights);
+		return apply_weights(sub_view(img, range), weights);
+	}
+
+
+	u8 gauss3(gray::image_t const& img, u32 x, u32 y)
+	{
+		constexpr auto rw = [](u8 w) { return w / 16.0f; };
+		constexpr std::array<r32, 9> gauss
+		{
+			rw(1), rw(2), rw(1),
+			rw(2), rw(4), rw(2),
+			rw(1), rw(2), rw(1),
+		};
+
+		auto p = weighted_center(img, x, y, gauss);
+
+		assert(p >= 0.0f);
+		assert(p <= 255.0f);
+
+		return static_cast<u8>(p);
 	}
 
 
@@ -237,6 +265,27 @@ namespace libimage
 		};
 
 		auto p = weighted_center(view, x, y, gauss);
+
+		assert(p >= 0.0f);
+		assert(p <= 255.0f);
+
+		return static_cast<u8>(p);
+	}
+
+
+	u8 gauss5(gray::image_t const& img, u32 x, u32 y)
+	{
+		constexpr auto rw = [](u8 w) { return w / 256.0f; };
+		constexpr std::array<r32, 25> gauss
+		{
+			rw(1), rw(4),  rw(6),  rw(4),  rw(1),
+			rw(4), rw(16), rw(24), rw(16), rw(4),
+			rw(6), rw(24), rw(36), rw(24), rw(6),
+			rw(4), rw(16), rw(24), rw(16), rw(4),
+			rw(1), rw(4),  rw(6),  rw(4),  rw(1),
+		};
+
+		auto p = weighted_center(img, x, y, gauss);
 
 		assert(p >= 0.0f);
 		assert(p <= 255.0f);
@@ -266,6 +315,19 @@ namespace libimage
 	}
 
 
+	r32 x_gradient(gray::image_t const& img, u32 x, u32 y)
+	{
+		constexpr std::array<r32, 9> kernel
+		{
+			1.0f, 0.0f, -1.0f,
+			2.0f, 0.0f, -2.0f,
+			1.0f, 0.0f, -1.0f,
+		};
+
+		return weighted_center(img, x, y, kernel);
+	}
+
+
 	r32 x_gradient(gray::view_t const& view, u32 x, u32 y)
 	{
 		constexpr std::array<r32, 9> kernel
@@ -276,6 +338,19 @@ namespace libimage
 		};
 
 		return weighted_center(view, x, y, kernel);
+	}
+
+
+	r32 y_gradient(gray::image_t const& img, u32 x, u32 y)
+	{
+		constexpr std::array<r32, 9> kernel
+		{
+			 1.0f,  2.0f,  1.0f,
+			 0.0f,  0.0f,  0.0f,
+			-1.0f, -2.0f, -1.0f,
+		};
+
+		return weighted_center(img, x, y, kernel);
 	}
 
 
--- libimage/proc/convolve.hpp
+++ libimage/proc/convolve.hpp
@@ -4,11 +4,22 @@
 
 namespace libimage
 {
+	u8 gauss3(gray::image_t const& img, u32 x, u32 y);
+
 	u8 gauss3(gray::view_t const& view, u32 x, u32 y);
+
+
+	u8 gauss5(gray::image_t const& img, u32 x, u32 y);
 
 	u8 gauss5(gray::view_t const& view, u32 x, u32 y);
 
+
+	r32 x_gradient(gray::image_t const& img, u32 x, u32 y);
+
 	r32 x_gradient(gray::view_t const& view, u32 x, u32 y);
+
+
+	r32 y_gradient(gray::image_t const& img, u32 x, u32 y);
 
 	r32 y_gradient(gray::view_t const& view, u32 x, u32 y);
 }
