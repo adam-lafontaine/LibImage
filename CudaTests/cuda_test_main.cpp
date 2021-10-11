fa4e063bddb7c8ee4207531effec251e403951a0
adam-lafontaine <adam.lafontaine@gmail.com>
2021-10-11 06:21:56 -0400

    read and write

--- CudaTests/cuda_test_main.cpp
+++ CudaTests/cuda_test_main.cpp
@@ -6,7 +6,8 @@
 namespace fs = std::filesystem;
 namespace img = libimage;
 
-constexpr auto ROOT_DIR = "~/Repos/LibImage/CudaTests";
+//constexpr auto ROOT_DIR = "~/Repos/LibImage/CudaTests";
+constexpr auto ROOT_DIR = "/home/adam/Repos/LibImage/CudaTests";
 
 const auto ROOT_PATH = fs::path(ROOT_DIR);
 
@@ -18,10 +19,15 @@ const auto DST_IMAGE_ROOT = ROOT_PATH / "out_files";
 
 int main()
 {
-    printf("hello from main\n");
+    printf("\nHello from main\n\n");
+
+
 
     img::image_t src;
     img::read_image_from_file(CORVETTE_PATH, src);
+    //img::make_image(src, 500, 500);
 
-    img::write_image(src, DST_IMAGE_ROOT / "corvette_out.png");
+    img::write_image(src, DST_IMAGE_ROOT / "corvette_out.png");    
+
+    printf("\nDone.\n");
 }
