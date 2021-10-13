84307be9911f9f544dc95877e01f0a804f666061
adam-lafontaine <adam.lafontaine@gmail.com>
2021-10-13 16:49:13 -0400

    image positions

--- libimage/cuda/blur.cu
+++ libimage/cuda/blur.cu
@@ -15,7 +15,7 @@ constexpr int THREADS_PER_BLOCK = 1024;
 
 
 GPU_FUNCTION
-bool is_top_row(u32 width, u32 height, u32 i)
+bool is_outer_top(u32 width, u32 height, u32 i)
 {
     // i = y * width + x
     return i < width;
@@ -23,36 +23,80 @@ bool is_top_row(u32 width, u32 height, u32 i)
 
 
 GPU_FUNCTION
-bool is_bottom_row(u32 width, u32 height, u32 i)
+bool is_outer_bottom(u32 width, u32 height, u32 i)
 {    
-    u32 row_end = height * width;
-    u32 row_begin = row_end - width;
+    u32 end = height * width;
+    u32 begin = end - width;
 
-    return i >= row_begin && i < row_end;
+    return i >= begin && i < end;
 }
 
 
 GPU_FUNCTION
-bool is_left_column(u32 width, u32 height, u32 i)
+bool is_outer_left(u32 width, u32 height, u32 i)
 {
     return i % width == 0;
 }
 
 
 GPU_FUNCTION
-bool is_right_column(u32 width, u32 height, u32 i)
+bool is_outer_right(u32 width, u32 height, u32 i)
 {
     return (i + 1) % width == 0;
 }
 
 
 GPU_FUNCTION
+bool is_inner_top(u32 width, u32 height, u32 i)
+{
+    u32 begin = width + 1;
+    u32 end = 2 * width - 1;
+
+    return i >= begin && i < end;
+}
+
+
+GPU_FUNCTION
+bool is_inner_bottom(u32 width, u32 height, u32 i)
+{
+    u32 end = width * (height - 1) - 1;
+    u32 begin = end - width + 1;
+
+    return i >= begin && i < end;
+}
+
+
+GPU_FUNCTION
+bool is_inner_left(u32 width, u32 height, u32 i)
+{
+    return i > 0 && (i - 1) % width == 0;
+}
+
+
+GPU_FUNCTION
+bool is_inner_right(u32 width, u32 height, u32 i)
+{
+    return (i + 2) % width == 0;
+}
+
+
+GPU_FUNCTION
 bool is_outer_edge(u32 width, u32 height, u32 i)
 {
-    return is_top_row(width, height, i) || 
-        is_bottom_row(width, height, i) || 
-        is_left_column(width, height, i) || 
-        is_right_column(width, height, i);
+    return is_outer_top(width, height, i) || 
+        is_outer_bottom(width, height, i) || 
+        is_outer_left(width, height, i) || 
+        is_outer_right(width, height, i);
+}
+
+
+GPU_FUNCTION
+bool is_inner_edge(u32 width, u32 height, u32 i)
+{
+    return is_inner_top(width, height, i) || 
+        is_inner_bottom(width, height, i) || 
+        is_inner_left(width, height, i) || 
+        is_inner_right(width, height, i);
 }
 
 
@@ -71,7 +115,11 @@ namespace libimage
 
         if(is_outer_edge(width, height, i))
         {
-            dst[i] = src[i];
+            dst[i] = 0; // src[i];
+        }
+        else if(is_inner_edge(width, height, i))
+        {
+            dst[i] = 75;
         }
         else
         {
