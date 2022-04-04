d661c318effc161deee4812812eecad82c11b358
adam-lafontaine <adam.lafontaine@gmail.com>
2022-04-04 19:01:14 -0400

    sequential color images

--- libimage/proc/binarize.cpp
+++ libimage/proc/binarize.cpp
@@ -132,6 +132,39 @@ namespace libimage
 			seq::transform_self(src_dst, conv);
 		}
 
+
+#ifndef LIBIMAGE_NO_COLOR
+
+		void binarize(image_t const& src, gray::image_t const& dst, pixel_to_bool_f const& cond)
+		{
+			auto const conv = [&](pixel_t p) { return cond(p) ? 255 : 0; };
+			seq::transform(src, dst, conv);
+		}
+
+
+		void binarize(image_t const& src, gray::view_t const& dst, pixel_to_bool_f const& cond)
+		{
+			auto const conv = [&](pixel_t p) { return cond(p) ? 255 : 0; };
+			seq::transform(src, dst, conv);
+		}
+
+
+		void binarize(view_t const& src, gray::image_t const& dst, pixel_to_bool_f const& cond)
+		{
+			auto const conv = [&](pixel_t p) { return cond(p) ? 255 : 0; };
+			seq::transform(src, dst, conv);
+		}
+
+
+		void binarize(view_t const& src, gray::view_t const& dst, pixel_to_bool_f const& cond)
+		{
+			auto const conv = [&](pixel_t p) { return cond(p) ? 255 : 0; };
+			seq::transform(src, dst, conv);
+		}
+
+
+#endif // !LIBIMAGE_NO_COLOR
+
 #endif // !LIBIMAGE_NO_GRAYSCALE
 	}
 
--- libimage/proc/binarize.hpp
+++ libimage/proc/binarize.hpp
@@ -57,20 +57,21 @@ namespace libimage
 
 		void binarize_self(gray::view_t const& src_dst, u8_to_bool_f const& cond);
 
+
+#ifndef LIBIMAGE_NO_COLOR
+
+		void binarize(image_t const& src, gray::image_t const& dst, pixel_to_bool_f const& cond);
+
+		void binarize(image_t const& src, gray::view_t const& dst, pixel_to_bool_f const& cond);
+
+		void binarize(view_t const& src, gray::image_t const& dst, pixel_to_bool_f const& cond);
+
+		void binarize(view_t const& src, gray::view_t const& dst, pixel_to_bool_f const& cond);
+
+
+#endif // !LIBIMAGE_NO_COLOR
+
 #endif // !LIBIMAGE_NO_GRAYSCALE
 	}
 
-
-#ifndef LIBIMAGE_NO_SIMD
-
-	namespace simd
-	{
-#ifndef LIBIMAGE_NO_GRAYSCALE
-
-
-
-#endif // !LIBIMAGE_NO_GRAYSCALE
-	}
-
-#endif // !LIBIMAGE_NO_SIMD
 }
